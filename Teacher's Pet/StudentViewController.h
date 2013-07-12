4fe4f81f1caf78e029393168528beee3597cfcf7
Angela Zhang <angelafzhang@gmail.com>
2013-07-11 22:35:54 -0700

    Need to get reloadData working to display contents of table; studentViewController should display otherwise but need to get and initialize data from heroku (or whatever)

--- Teacher's Pet/StudentViewController.h
+++ Teacher's Pet/StudentViewController.h
@@ -9,5 +9,6 @@
 #import <UIKit/UIKit.h>
 
 @interface StudentViewController : UIViewController
-
+- (void)exitView;
+- (float)difficulty; //returns current difficult of slider bar
 @end
---
+++ Teacher's Pet/Task.m
@@ -0,0 +1,15 @@
+//
+//  Task.m
+//  Teacher's Pet
+//
+//  Created by Angela Zhang on 7/11/13.
+//  Copyright (c) 2013 Ada Taylor. All rights reserved.
+//
+
+#import "Task.h"
+
+@implementation Task
+
+- (id)init
+
+@end
