f2172396df454c3cdb2c08c856b78ffce745ff75
Ada Taylor <ada@ada-mbp.thefacebook.com>
2013-07-11 17:13:40 -0700

    Initial Commit

---
+++ Teacher's Pet/TeachersPetAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TeachersPetAppDelegate.h
+//  Teacher's Pet
+//
+//  Created by Ada Taylor on 7/11/13.
+//  Copyright (c) 2013 Ada Taylor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TeachersPetAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Teacher's Pet/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Teacher's Pet
+//
+//  Created by Ada Taylor on 7/11/13.
+//  Copyright (c) 2013 Ada Taylor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TeachersPetAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TeachersPetAppDelegate class]));
+    }
+}
